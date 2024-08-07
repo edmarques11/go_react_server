e94919d79d890be0991453dac6d64fc05cc5bd3f
Edmarques Lima <edmarquessantosdelima@gmail.com>
2024-08-06 23:21:37 -0300

    feat: configure api

--- internal/store/pgstore/migrations/002_create_messages_table.sql
+++ internal/store/pgstore/migrations/002_create_messages_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS messages(
   "id" uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   "room_id" uuid NOT NULL,
-  "message" varchar(255) NOT NULL,
+  "message" VARCHAR(255) NOT NULL,
   "reaction_count" BIGINT NOT NULL DEFAULT 0,
   "answered" BOOLEAN NOT NULL DEFAULT FALSE,
 
-  foreign key ("room_id") references rooms("id")
+  FOREIGN KEY ("room_id") REFERENCES rooms("id")
 );
 ---- create above / drop below ----
 DROP TABLE IF EXISTS messages;
